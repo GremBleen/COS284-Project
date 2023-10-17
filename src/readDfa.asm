27f01a8d4a020840b0dd195e0d50602642d606dd
Acedem <aidanchappie@icloud.com>
2023-10-17 15:17:53 +0200

    Deliverable 1 L1 and L2 implemented

--- src/readDfa.asm
+++ src/readDfa.asm
@@ -9,7 +9,141 @@ section .text
 
 getNextNumber:
     ; INPUT:
-    ; rdi = 
+    ; rdi = rdi
+    ; rsi = rsi
+    ; rdx = [rsp + filedata]
+    ; rbx = rbx
+    ; rcx = rcx
+
+    extern malloc, free
+
+    push rbp      ; Save the base pointer
+    mov  rbp, rsp ; Set the base pointer to the stack pointer
+    sub rsp, 64
+
+    temp equ 0
+    rdi_backup equ 8
+    rsi_backup equ 16
+    rbx_backup equ 24
+    rcx_backup equ 32
+    rax_backup equ 40
+    r12_backup equ 48
+    r13_backup equ 56
+
+    mov [rsp + r12_backup], r12
+    mov [rsp + r13_backup], r13
+
+    ; This loop is used to dertermine how many characters makes up the number of states
+    xor r8, r8 ; clearing r8
+    mov r8, rcx ; setting r8 to rcx
+
+    l1_loop_1:
+        mov al, [rdi + r8]
+        cmp al, ','
+        je l1_loop_1_end
+        cmp al, 10 ; '\n'
+        je l1_loop_1_end
+        cmp al, '0'
+        jl file_error
+        cmp al, '9'
+        jg file_error
+        inc r8
+        jmp l1_loop_1
+    l1_loop_1_end:
+
+    cmp r8, rcx
+
+    je file_error
+
+    ; saving values before function call
+    mov [rsp + rdi_backup], rdi
+    mov [rsp + rsi_backup], rsi
+    mov [rsp + rbx_backup], rbx
+    mov [rsp + rcx_backup], rcx
+
+    ; moving the value of r8 into a callee saved register
+    mov r13, r8
+    sub r13, rcx
+
+    ; calling malloc
+    mov rdi, r13
+    ; Allocating memory for each byte
+    call malloc
+    mov [rsp + temp], rax
+
+    ; restoring values from stack
+    mov rdi, [rsp + rdi_backup]
+    mov rsi, [rsp + rsi_backup]
+    mov rbx, [rsp + rbx_backup]
+    mov rcx, [rsp + rcx_backup]
+
+    ; loop through it until ',' again, adding each digit to the array
+    
+    xor r9, r9 ; Setting r9 to 0
+    
+    l1_loop_2:
+        mov al, [rdi + rcx]
+        cmp al, ','
+        je l1_loop_2_end
+        cmp al, 10 ; '\n'
+        je l1_loop_2_end
+        mov r10, [rsp + temp]
+        mov byte[r10 + r9], al
+        inc r9
+        inc rcx
+        jmp l1_loop_2
+    l1_loop_2_end:
+
+    ; loop through each of the digits, subtracting '0', multiply each by 10^n and add
+    xor r8, r8 ; clear r8
+    dec r13 ; decrement r13 so that it aligns with indices
+    xor r11, r11 ; clearing r11 -> counter
+    xor r9, r9
+    mov r9, [rsp + temp]
+
+    xor rax, rax ; clear rax - will be used as accumulator
+    
+    l1_loop_3:
+        cmp r13, 0
+        jl l1_loop_3_end
+        mov r8b, byte[r9 + r13]
+        sub r8, '0'
+        mov r10, 0
+        l1_loop_3_1:
+            cmp r10, r11
+            jge l1_loop_3_1_end
+            imul r8, 10
+            inc r10
+            jmp l1_loop_3_1
+        l1_loop_3_1_end:
+        add rax, r8 ; add r8 to rax
+        inc r11
+        dec r13
+        jmp l1_loop_3
+    l1_loop_3_end:
+    
+    ; saving values to stack
+    mov [rsp + rdi_backup], rdi
+    mov [rsp + rsi_backup], rsi
+    mov [rsp + rbx_backup], rbx
+    mov [rsp + rcx_backup], rcx
+    mov [rsp + rax_backup], rax
+
+    ; freeing temp
+    mov rdi, [rsp + temp]
+    call free
+
+    ; restoring values from stack
+    mov rdi, [rsp + rdi_backup]
+    mov rsi, [rsp + rsi_backup]
+    mov rbx, [rsp + rbx_backup]
+    mov rcx, [rsp + rcx_backup]
+    mov rax, [rsp + rax_backup]
+    mov r12, [rsp + r12_backup]
+    mov r13, [rsp + r13_backup]
+
+    leave
+    ret
 
 readDfa:
     ; DFA* readDfa(const char *filename)
@@ -17,20 +151,22 @@ readDfa:
     ;Input registers:
     ; rdi = filename
 
-    extern open, lseek, malloc, free, read, close
+    extern open, lseek, read, close
     push rbp      ; Save the base pointer
     mov  rbp, rsp ; Set the base pointer to the stack pointer
-    sub rsp, 64
+    sub rsp, 80
 
     r12_data equ 0
     states equ 8
     transitions equ 16
-    numStates equ 20
-    numTransitions equ 24
-    startState equ 28
-    filedata equ 32
-    temp equ 40
+    numStates equ 24
+    numTransitions equ 28
+    startState equ 32
+    filedata equ 40
     r13_data equ 48
+    rcx_backup1 equ 56
+    rsi_backup1 equ 64
+    rdi_backup1 equ 72
 
     ; saving the value stored in r12
     mov [rsp + r12_data], r12
@@ -109,11 +245,10 @@ readDfa:
         mov rdi, [rsp+filedata] ; rdi -> pointer to the first bit in the file
         xor rbx, rbx ; rbx -> row counter
 
+
         while:
             cmp rcx, rsi
             jge _endwhile
-            ; mov al, [rdi+rcx] ; essentially getting the the ith bit of the file
-            ; TODO: populate and create dfa using the data in here
 
             mov r8, rbx
             cmp r8, 0
@@ -131,113 +266,95 @@ readDfa:
 
             ; case l1 is for the first line (Number of states and Number of transitions)
             l1:
-                ; This loop is used to dertermine how many characters makes up the number of states
-                xor r8, r8 ; clearing r8
-                mov r8, rcx ; setting r8 to rcx
-  
-                l1_loop_1:
-                    mov al, [rdi+r8]
-                    cmp al, ','
-                    je l1_loop_1_end
-                    cmp al, '0'
-                    jl file_error
-                    cmp al, '9'
-                    jg file_error
-                    inc r8
-                    jmp l1_loop_1
-                l1_loop_1_end:
 
-                cmp r8, rcx
-
+                mov rdx, [rsp + filedata]
+                call getNextNumber
+                mov dword[rsp + numStates], eax
+                mov al, byte[rdi + rcx]
+                cmp al, 10 ; '\n'
                 je file_error
-
-                ; moving the value of rcx into a callee saved register
-                mov r12, rcx
-
-                ; moving the value of r8 into a callee saved register
-                mov r13, r8
-
-                ; calling malloc
-                mov rdi, r8
-                ; Allocating memory for each byte
-                call malloc
-                mov [rsp+temp], rax
-
-                mov rdi, [rsp + filedata] ; resetting rdi to hold the file
-                mov rcx, r12 ; loading the value stored in r12 back into rcx
-
-                ; loop through it until ',' again, adding each digit to the array
+                inc rcx ; increment rcx to get past ',' or '\n'
                 
-                xor r9, r9 ; Setting r9 to 0
-                
-                l1_loop_2:
-                    mov al, [rdi + rcx]
-                    cmp al, ','
-                    je l1_loop_2_end
-                    mov r10, [rsp + temp]
-                    mov byte[r10 + r9], al
-                    inc r9
-                    inc rcx
-                    jmp l1_loop_2
-                l1_loop_2_end:
-                
-                ; increment rcx to get past comma
+                mov rdx, [rsp + filedata]
+                call getNextNumber
+                mov dword[rsp + numTransitions], eax
+                mov al, byte[rdi + rcx]
+                cmp al, ','
+                je file_error
                 inc rcx
 
-                ; loop through each of the digits, subtracting '0', multiply each by 10^n and add
-                xor r8, r8 ; clear r8
-                dec r13 ; decrement r13 so that it aligns with indices
-                xor r11, r11 ; clearing r11 -> counter
-                xor r9, r9
-                mov r9, [rsp + temp]
-
-                xor rax, rax ; clear rax - will be used as accumulator
-                
-                l1_loop_3:
-                    cmp r13, 0
-                    jl l1_loop_3_end
-                    mov r8b, byte[r9 + r13]
-                    sub r8, '0'
-                    mov r10, 0
-                    l1_loop_3_1:
-                        cmp r10, r11
-                        jge l1_loop_3_1_end
-                        imul r8, 10
-                        inc r10
-                        jmp l1_loop_3_1
-                    l1_loop_3_1_end:
-                    add rax, r8 ; add r8 to rax
-                    inc r11
-                    dec r13
-                    jmp l1_loop_3
-                l1_loop_3_end:
-
-                mov [rsp + numStates], rax
-
-                ; free the memory allocated
-                mov rdi, [rsp+temp]
-                call free
+                mov eax, dword[rsp + numStates]
+                mov eax, dword[rsp + numTransitions]
 
                 inc rbx ; increment row
                 jmp end_switch
 
             ; case l2 is for the second line (States)
             l2:
-                ; loop for line 2 while rcx != '\n'
+                xor r12, r12
+                xor r13, r13
+                xor rax, rax
+                mov eax, [rsp + numStates]
+                mov r13, rax
+                cmp r12, r13
+                jg file_error
+
+                ; need to backup non-callee saved registers
+                mov [rsp + rcx_backup1], rcx
+                mov [rsp + rsi_backup1], rsi
+                mov [rsp + rdi_backup1], rdi
+
+                mov edi, [rsp + numStates]
+                imul rdi, 4 ; integer = 4 bytes
+                call malloc
+                mov [rsp + states], rax
+                
+                ; resetting values
+                mov rcx, [rsp + rcx_backup1]
+                mov rsi, [rsp + rsi_backup1]
+                mov rdi, [rsp + rdi_backup1]
+
+                l2_loop_1:
+                    cmp r12, r13
+                    je l2_loop_1_end
+                    mov rdx, [rsp + filedata]
+                    call getNextNumber
+
+                    mov r8, [rsp + states]
+                    mov [r8 + r12*4], eax
+
+                    inc r12
+                    mov al, byte[rdi + rcx]
+                    cmp al, 10 ; '\n'
+                    jne end_if_1
+                    if:
+                        cmp r12, r13
+                        jne file_error
+                        jmp l2_loop_1
+                    end_if_1:
+                    inc rcx
+                    jmp l2_loop_1
+                l2_loop_1_end:
+
+                mov al, byte[rdi + rcx]
+                cmp al, 10 ; '\n'
+                jne file_error
+
+                inc rcx
 
                 inc rbx ; increment row
                 jmp end_switch
             
             ; case l3 is for the third line (Accepting states)
             l3:
-                ; loop for line 3 while rcx != '\n'
+                
 
                 inc rbx ; increment row
                 jmp end_switch
 
             ; case ln is for the nth line (Transitions)
             ln:
-                ; loop for line n while rcx != '\n'
+                
 
                 inc rbx ; increment row
 
@@ -246,6 +363,16 @@ readDfa:
             jmp while
 
         _endwhile:
+
+        ; setting startState to 0
+        mov rax, 0
+        mov [rsp + startState], rax
+
+        ; combining everything into Dfa:
+
+        ; use initDfa
+
+        ; cleanup - free all malloc'd data
 
         ; closing the file
         mov edi, [fd]
