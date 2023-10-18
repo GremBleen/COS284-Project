5f7d9ebcb7c763b186c1776280ed498dfa60032b
Acedem <aidanchappie@icloud.com>
2023-10-18 15:07:18 +0200

    Deriverable 1 finished and working

--- src/main.c
+++ src/main.c
@@ -10,16 +10,13 @@ extern DFA *readDfa(const char *filename);
 
 int main()
 {
-    char* filename = "dfa1.txt";
-    DFA *temp = readDfa(filename);
+    if (testDeliverable1() < 29 || testDeliverable2() < 29)
+    {
+        printf("Warning: Previous Deliverables are not fully correct and might affect Deliverable 3\n");
+    }
 
-    // if (testDeliverable1() < 29 || testDeliverable2() < 29)
-    // {
-    //     printf("Warning: Previous Deliverables are not fully correct and might affect Deliverable 3\n");
-    // }
-
-    // float marks = testDeliverable3();
-    // printf("Total Marks %.f\n", marks);
+    float marks = testDeliverable3();
+    printf("Total Marks %.f\n", marks);
 
     return 0;
 }
